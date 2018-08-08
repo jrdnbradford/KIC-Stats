46ca6ba6d34f72298c03d8f72524446ce9380b5a
Jordan Bradford <36420801+jrdnbradford@users.noreply.github.com>
2018-08-08 10:40:58 -0400

    Refactor of project.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# kic-data
+# kic-stats
 The code in this repo takes the daily usage statistics XML data output by Knowledge Imaging Center (KIC) scanner software (version 4.2.0.0) and displays it in a web browser for general analysis that may help inform business/budgetary decisions and KIC configuration settings. 
 
-It should be known that the KIC Fleet Manager software available for purchase from Image Access provides much more functionality and that the code in this repo is meant to be an open-source alternative for institutions that wish to have a way to analyze the  data without the cost (and all the extra features) that the Fleet Manager software provides.
+It should be known that the KIC Fleet Manager software available for purchase from Image Access provides much more functionality and that the code in this repo is meant to be an open-source alternative for institutions that wish to have a way to look at a cleaned up version of important data without the cost (and all the extra features) that the Fleet Manager software provides.
 
 See the following sites for more information about KIC scanners:
 
@@ -12,30 +12,9 @@ See the following sites for more information about KIC scanners:
 ## Using the Code
 This front-end project was built with basic HTML, CSS, and vanilla JS. Loading the XML data from your computer will require that you run a local web server as browsers restrict cross-origin HTTP requests initiated from within local scripts as a security precaution.
 
-Configure the KIC scanner to send the daily statistics email in KIC Setup. Download the zip file with the XML data that is attached to the email, unzip the file, rename the file to whatever you wish, and add the .xml file extension. The script will create a data table for as many XML files as you want, with columns that indicate basic data such as: 
+Configure the KIC scanner to send the daily statistics email in KIC Setup. Download the zip file with the XML data that is attached to the email, unzip the file, and rename the file to "yourfilename.xml". Ensure that the name of the file is coded into the array variable "XMLFiles" in [script.js](script.js). Repeat this process with as many XML files as you wish. Note that the script currently looks for the XML files within a directory entitled "XML".
 
-* Date
-* Amount of Data Sent in MegaBytes
-* Total Number of Scans
-* Total Number of Sessions
-* Average Number of Scans per Session
-
-Comments in the JS file will assist with setup. Look for ATTENTION warnings in the code:
-
-### ATTENTION #1
-___If you only wish to see the basic data described above, this may be the only area that requires your attention___. In the code already in place, the script file will look for a folder named "XML" located in the same folder as the script file and grab the files named bookeye1.xml, bookeye2.xml, and bookeye3.xml within the XML folder. Add or remove as many XML files as you like from the XMLFiles array and the script will take care of the rest (if the files are named correctly, match the file names in the script, and are in the correct location).
-
-### ATTENTION #2 
-If you wish to add more columns with different data sets, you must add the new headers into the tableHeaderAry array as strings.
-
-### ATTENTION #3 
-More data from the XML files can be added here if needed.
-
-### ATTENTION #4 
-The heavyUseNum and lightUseNum arguments in calls to appendDataToTable() should be edited based on user needs.
-
-### ATTENTION #5 
-If more headers were added to tableHeaderAry (ATTENTION #2), the code for adding the new columns should be added here in the correct order.
+The script can create two types of tables. One includes the amount of data sent, total number of scans, total number of sessions, and the average number of scans per session for each date that the KIC was able to write that day's data. The other is a high level view that looks at the entirety of the recorded data in the file and provides some highlights.
 
 ## Authors
 **Jordan Bradford** - GitHub: [jrdnbradford](https://github.com/jrdnbradford)
---
+++ assets/script.js
@@ -0,0 +1,203 @@
+"use strict";
+
+var XMLFiles = [
+    'XML/bookeye1.xml'/*,
+    'XML/bookeye2.xml', 
+    'XML/bookeye3.xml'*/
+]; //Add file paths here
+
+getXML(createExhaustiveTable);
+
+let exhaustiveDetailBTN = document.getElementById('ExhaustiveDetail'); //button
+let glanceBTN = document.getElementById('Total'); //button
+let DIV = document.getElementById('Stats'); //div 
+
+exhaustiveDetailBTN.addEventListener('click', function() {
+    removeChildren();
+    getXML(createExhaustiveTable);
+});
+
+glanceBTN.addEventListener('click', function() {
+    removeChildren();
+    getXML(createGlanceRundown);
+});
+
+function appendDataToRow(data, newTableRow) {
+    let newTableData = document.createElement('td');
+    newTableData.textContent = data;
+    newTableRow.appendChild(newTableData);
+} //Creates table data element, sets its content to whatever data is passed, and appends it
+
+function appendHeader(headerAry, newTableRow) {
+    headerAry.forEach(function(header) {
+        let newTableHeader = document.createElement('th');
+        newTableHeader.textContent = header;
+        newTableRow.appendChild(newTableHeader);
+    }); 
+} //Appends table header element for each header in passed array to create columns
+
+function getAvg(array) {
+    let runningTotal = 0;
+    for (let i = 0; i < array.length; i++) {
+        let number = parseInt(array[i].textContent);
+        runningTotal = runningTotal + number;  
+   }
+    let avg = Math.round(runningTotal / array.length);
+    return avg;
+} //Returns avg number of scans for a particular KIC
+
+function getDate(date) {
+    let day = date.slice(8, 10);
+    let month = date.slice(5, 7);
+    let year = date.slice(2, 4);
+    date = month + "/" + day + "/" + year;
+    return date;
+} //Returns reader friendly date
+
+function getHighestNum(array) {
+    let highestNum = 0;
+    for (let i = 0; i < array.length; i++) {
+        let potentialHighNum = parseInt(array[i].textContent);
+        if (potentialHighNum > highestNum) {
+            highestNum = potentialHighNum;
+        }
+    }
+    return highestNum; 
+} //Returns highest number in passed array
+
+function getRunningTotal(array) {
+    let runningTotal = 0;
+    for (let i = 0; i < array.length; i++) {
+        let number = parseInt(array[i].textContent);
+        runningTotal = runningTotal + number;
+   }
+   return runningTotal;
+} //Returns total scans for a particular KIC
+
+function getXML(funct) {
+    XMLFiles.forEach(function(XMLFile) {
+        let XHR = new XMLHttpRequest();
+        XHR.onreadystatechange = funct; 
+        XHR.open('GET', XMLFile);
+        XHR.send();
+    }); //This may cause the script to create tables out of order in some browsers
+} //Use AJAX to get XML data from each file in the XMLFiles array and run passed function
+
+function removeChildren() {
+   while (DIV.firstChild) {
+       DIV.removeChild(DIV.firstChild);
+   }   
+} //Removes all elements nested within <div id="Stats">
+
+//Main functions
+function createExhaustiveTable() {
+    if (this.readyState === 4 && this.status === 404) {
+        alert("Something went wrong. See the Web Console for more details.");
+    } else if (this.readyState === 4 && this.status === 200) {
+        let newTable = document.createElement('table');
+        newTable.align = 'center';
+        DIV.appendChild(newTable); //Append table
+
+        newTable.addEventListener('click', (e) => {
+            if (e.target.nodeName == 'TD') {
+                if (e.target.parentNode.style.backgroundColor == 'yellow') {
+                        e.target.parentNode.style.backgroundColor = '';
+                } else {
+                        e.target.parentNode.style.backgroundColor = 'yellow';     
+                }
+            }
+        }); //Add event listener to table to highlight clicked table rows
+        
+        let compName = this.responseXML.getElementsByTagName('ComputerName')[0].textContent;
+        let newCaption = document.createElement('caption');
+        newCaption.textContent = compName;
+        newTable.appendChild(newCaption); //Append KIC computer name as caption
+
+        let newTableRow = document.createElement('tr');
+        newTable.appendChild(newTableRow); //Append table header row
+        
+        let tableHeaderAry = [
+            /*Column 1*/ "Date", 
+            /*Column 2*/ "Data Sent (MBs)", 
+            /*Column 3*/ "Total Scans",
+            /*Column 4*/ "Sessions", 
+            /*Column 5*/ "Avg Scans/Session" 
+        ]; 
+        
+        appendHeader(tableHeaderAry, newTableRow);
+
+        let dateAry = this.responseXML.getElementsByTagName('Date');
+        let sizeAry = this.responseXML.getElementsByTagName('Size');
+        let scansAry = this.responseXML.getElementsByTagName('Scans');
+        let totalSesAry = this.responseXML.getElementsByTagName('Ss');
+        
+        for (let i = 0; i < scansAry.length; i++) {
+            newTableRow = document.createElement('tr');
+            newTable.appendChild(newTableRow); //Append table row
+
+            let date = dateAry[i].textContent.slice(0, 10);
+            appendDataToRow(getDate(date), newTableRow); //Column 1
+            
+            //XML has strange size format that requires dividing by 100 to return size in megabytes
+            let size = parseInt(sizeAry[i].textContent); 
+            let sizeMB = Math.round(size / 100);
+            appendDataToRow(sizeMB, newTableRow); //Column 2
+
+            let totalScans = parseInt(scansAry[i].textContent);
+            appendDataToRow(totalScans, newTableRow); //Column 3
+
+            let totalSes = totalSesAry[i].textContent;
+            appendDataToRow(totalSes, newTableRow); //Column 4
+
+            let avgScansPerSes = Math.round(totalScans / totalSes);
+            appendDataToRow(avgScansPerSes, newTableRow); //Column 5
+        } //Each iteration of this loop creates a single row complete with data in the table
+    } 
+} 
+
+function createGlanceRundown() {
+    if (this.readyState === 4 && this.status === 404) {
+        alert("Something went wrong. See the Web Console for more details.");
+    } else if (this.readyState === 4 && this.status === 200) {
+        let newTable = document.createElement('table');
+        newTable.align = 'center';
+        DIV.appendChild(newTable); //Append table
+        
+        let compName = this.responseXML.getElementsByTagName('ComputerName')[0].textContent;
+        
+        let dateAry = this.responseXML.getElementsByTagName('Date');
+        let firstDate = dateAry[0].textContent.slice(0, 10);
+        let lastDate = dateAry[dateAry.length - 1].textContent.slice(0, 10);
+        let dateRange = getDate(firstDate) + " - " + getDate(lastDate);
+        
+        let newCaption = document.createElement('caption');
+        newCaption.textContent = compName + " (" + dateRange + ")";
+        newTable.appendChild(newCaption); //Append KIC computer name and date range as caption 
+
+        let newTableRow = document.createElement('tr');
+        newTable.appendChild(newTableRow); //Append table header row
+
+        let tableHeaderAry = [
+            /*Column 1*/ "Data Sent (MBs)",
+            /*Column 2*/ "Avg Data/Day (MBs)",
+            /*Column 3*/ "Total Scans",
+            /*Column 4*/ "Avg Scans/Day",
+            /*Column 5*/ "Highest # of Scans in a Day"
+        ];
+        
+        appendHeader(tableHeaderAry, newTableRow); //Append header row
+
+        newTableRow = document.createElement('tr');
+        newTable.appendChild(newTableRow); //Append table row
+
+        //XML has strange size format that requires dividing by 100 to return size in megabytes
+        let sizeAry = this.responseXML.getElementsByTagName('Size');
+        appendDataToRow(Math.round((getRunningTotal(sizeAry) / 100)), newTableRow); //Column 1
+        appendDataToRow(Math.round((getAvg(sizeAry) / 100)), newTableRow); //Column 2
+
+        let scansAry = this.responseXML.getElementsByTagName('Scans');
+        appendDataToRow(getRunningTotal(scansAry), newTableRow); //Column 3
+        appendDataToRow(getAvg(scansAry), newTableRow); //Column 4
+        appendDataToRow(getHighestNum(scansAry), newTableRow); //Column 5
+    }
+}
